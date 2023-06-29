6e8bbb3a96028b99f63ee6ecc009ecc1e0879932
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-06-29 04:46:41 +0300

    Done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,23 @@
 class ItemsController < ApplicationController
-end
+    def index
+      items = Item.all
+      render json: items.map { |item| item_json(item) }
+    end
+  
+    private
+  
+    def item_json(item)
+      {
+        id: item.id,
+        name: item.name,
+        description: item.description,
+        price: item.price,
+        user: {
+          id: item.user.id,
+          username: item.user.username,
+          city: item.user.city
+        }
+      }
+    end
+  end
+  
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+    def show
+      user = User.find(params[:id])
+      render json: {
+        id: user.id,
+        username: user.username,
+        city: user.city,
+        items: user.items.map { |item| item_json(item) }
+      }
+    end
+  
+    private
+  
+    def item_json(item)
+      {
+        id: item.id,
+        name: item.name,
+        description: item.description,
+        price: item.price
+      }
+    end
 end
+  
